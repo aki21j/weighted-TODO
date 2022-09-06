b2d7f778435e1b0c99826da5df900f6a9fc44e34
Ankit Gupta <ankit@Ankits-MacBook-Pro.local>
2022-09-07 00:57:59 +0100

    basic templates

--- w_todo_app/views.py
+++ w_todo_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TodoList, TodoItem
 
-# Create your views here.
+class ListListView(ListView):
+    model = TodoList
+    template_name = "w_todo_app/index.html"
+
+class ItemListView(ListView):
+    model = TodoItem
+    template_name = "w_todo_app/todo_list.html"
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = TodoList.objects.get(id=self.kwargs["list_id"])
+        return context
+
